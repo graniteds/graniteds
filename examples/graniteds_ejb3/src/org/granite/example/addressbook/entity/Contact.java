cc1bc393d232e5777701842e40ba1382918dd0b4
william <william.drai@graniteds.org>
2011-07-11 17:11:27 +0200

    Upgraded EJB3 example

---
+++ examples/graniteds_ejb3/src/org/granite/example/addressbook/entity/Contact.java
@@ -0,0 +1,94 @@
+/*
+  GRANITE DATA SERVICES
+  Copyright (C) 2011 GRANITE DATA SERVICES S.A.S.
+
+  This file is part of Granite Data Services.
+
+  Granite Data Services is free software; you can redistribute it and/or modify
+  it under the terms of the GNU Library General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or (at your
+  option) any later version.
+
+  Granite Data Services is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Library General Public License
+  for more details.
+
+  You should have received a copy of the GNU Library General Public License
+  along with this library; if not, see <http://www.gnu.org/licenses/>.
+*/
+
+package org.granite.example.addressbook.entity;
+
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+
+/**
+ * @author Franck WOLFF
+ */
+@Entity
+public class Contact extends AbstractEntity {
+
+    private static final long serialVersionUID = 1L;
+
+
+    @ManyToOne(optional=false)
+    private Person person;
+
+    @Basic
+    private String phone;
+    @Basic
+    private String mobile;
+    @Basic
+    private String fax;
+    @Basic
+    private String email;
+
+    @ManyToOne(cascade=CascadeType.ALL, optional=false)
+    private Address address;
+
+
+    public Person getPerson() {
+        return person;
+    }
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+    public void setFax(String fax) {
+        this.fax = fax;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
