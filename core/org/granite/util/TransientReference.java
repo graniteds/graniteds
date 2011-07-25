4bca4c6c177b515b62b8dd599d919df8023ee6a5
fwolff <fwolff@graniteds.org>
2011-07-25 12:21:16 -0400

    GDS-895: fix ServiceInvoker serialization in clustering environments.

---
+++ core/org/granite/util/TransientReference.java
@@ -0,0 +1,45 @@
+/*
+  GRANITE DATA SERVICES
+  Copyright (C) 2011 GRANITE DATA SERVICES S.A.S.
+
+  This file is part of Granite Data Services.
+
+  Granite Data Services is free software; you can redistribute it and/or modify
+  it under the terms of the GNU Library General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or (at your
+  option) any later version.
+
+  Granite Data Services is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Library General Public License
+  for more details.
+
+  You should have received a copy of the GNU Library General Public License
+  along with this library; if not, see <http://www.gnu.org/licenses/>.
+*/
+
+package org.granite.util;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * This annotation is intended to be used on classes that need to be cached in an HTTP session
+ * without being replicated in a clustered environment. It will however only work in conjunction
+ * with the session map returned by an <tt>HttpGraniteContext</tt> instance. 
+ * 
+ * @author Franck WOLFF
+ * 
+ * @see org.granite.messaging.webapp.HttpGraniteContext
+ * @see TransientReferenceHolder
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Inherited
+@Documented
+public @interface TransientReference {
+}
