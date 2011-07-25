5e049a736eb09b750099d1defe9191c5fb21408c
fwolff <fwolff@graniteds.org>
2011-07-25 12:23:04 -0400

    Simple filter forcing the creation of an HTTP session (clustering with sticky-session utility).

---
+++ core/org/granite/util/CreateSessionFilter.java
@@ -0,0 +1,52 @@
+/*
+  GRANITE DATA SERVICES
+  Copyright (C) 2011 GRANITE DATA SERVICES S.A.S.
+
+  This file is part of Granite Data Services.
+
+  Granite Data Services is free software; you can redistribute it and/or modify
+  it under the terms of the GNU Library General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or (at your
+  option) any later version.
+
+  Granite Data Services is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Library General Public License
+  for more details.
+
+  You should have received a copy of the GNU Library General Public License
+  along with this library; if not, see <http://www.gnu.org/licenses/>.
+*/
+
+package org.granite.util;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * This filter forces the creation of an HTTP session. Only useful in cluster
+ * environments when sticky-session is enabled, in order to ensure that a client
+ * will always use the same cluster node.
+ * 
+ * @author Franck WOLFF
+ */
+public class CreateSessionFilter implements Filter {
+
+	public void init(FilterConfig config) throws ServletException {
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		((HttpServletRequest)request).getSession(true);
+		chain.doFilter(request, response);
+	}
+
+	public void destroy() {
+	}
+}
