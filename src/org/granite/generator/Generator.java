d29c6daa4ffb9ed1797960331828f0303248dd0e
fwolff <fwolff@graniteds.org>
2012-02-16 12:32:22 -0500

    Version 2.3.1.GA

--- src/org/granite/generator/Generator.java
+++ src/org/granite/generator/Generator.java
@@ -37,7 +37,7 @@ public class Generator extends ArrayList<Transformer<?, ?, ?>> {
 
 	private static final long serialVersionUID = 1L;
 
-	public static final String VERSION = "2.3.0";
+	public static final String VERSION = "2.3.1";
 	
 	private Configuration config = null;
 
