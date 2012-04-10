316ebf2afd47f1ba4a469cddacfc4e2c12620b75
william <william.drai@graniteds.org>
2012-04-10 18:40:36 +0200

    Merge from branch gds3

---
+++ tide/org/granite/tide/Expression.java
@@ -0,0 +1,13 @@
+package org.granite.tide;
+
+
+public interface Expression {
+    
+    public String getComponentName();
+    
+    public String getComponentClassName();
+    
+    public String getExpression();
+    
+    public String getPath();
+}
