95425fb7a29f068c67937ffebf504c2185d50650
William Drai <william.drai@graniteds.org>
2012-10-22 13:06:10 +0200

    Improved EJB tests results formatting

---
+++ test/graniteds_test/java/org/granite/test/tide/ejb/TideOpenEJBTestSuite.java
@@ -0,0 +1,10 @@
+package org.granite.test.tide.ejb;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TideRemotingTest.class })
+public class TideOpenEJBTestSuite {
+}
