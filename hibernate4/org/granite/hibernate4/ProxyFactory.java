ad870f6809da5f7316c24fd44963fa4c40dca301
william <william.drai@graniteds.org>
2013-04-27 19:47:48 +0200

    GDS-1110

--- hibernate4/org/granite/hibernate4/ProxyFactory.java
+++ hibernate4/org/granite/hibernate4/ProxyFactory.java
@@ -68,7 +68,7 @@ public class ProxyFactory {
         	// superclass, getProxyFactory/getProxy methods are declared as static in each inherited
         	// class with the same signature.
             Class<?> initializerClass = TypeUtil.forName(initializerClassName);
-            getProxyFactory = initializerClass.getMethod("getProxyFactory", new Class[]{Class.class, Class[].class});
+            getProxyFactory = initializerClass.getMethod("getProxyFactory", new Class[] { Class.class, Class[].class });
             
             // Hibernate 4.0.1 has an extra boolean parameter in last position: classOverridesEquals.
             Method getProxy = null;
@@ -98,7 +98,7 @@ public class ProxyFactory {
         try {
             // Get ProxyFactory.
             Class<?> persistentClass = TypeUtil.forName(persistentClassName);
-            Class<?> factory = (Class<?>)getProxyFactory.invoke(null, new Object[]{persistentClass, INTERFACES});
+            Class<?> factory = (Class<?>)getProxyFactory.invoke(null, new Object[] { persistentClass, INTERFACES });
 
             // Convert id (if necessary).
             Object[] identifierInfo = getIdentifierInfo(persistentClass);
@@ -112,9 +112,9 @@ public class ProxyFactory {
             // Get Proxy (with or without the extra parameter classOverridesEquals)
             if (classOverridesEqualsParameter) {
                 return (HibernateProxy)getProxy.invoke(null, new Object[]{
-                	factory, entityName, persistentClass, INTERFACES, null, null, null, id, null, overridesEquals(persistentClass)});
+                	factory, entityName, persistentClass, INTERFACES, identifierGetter, null, null, id, null, overridesEquals(persistentClass)});
             }
-            return (HibernateProxy)getProxy.invoke(null, new Object[]{factory, entityName, persistentClass, INTERFACES, identifierGetter, null, null, id, null});
+            return (HibernateProxy)getProxy.invoke(null, new Object[] { factory, entityName, persistentClass, INTERFACES, identifierGetter, null, null, id, null });
         } 
         catch (Exception e) {
             throw new ServiceException("Error with proxy description: " + persistentClassName + '/' + entityName + " and id: " + id, e);
