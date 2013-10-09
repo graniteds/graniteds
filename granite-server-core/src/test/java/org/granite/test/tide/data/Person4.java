bb8eb3c021ac172a843b660df94840efbcd8aced
fwolff <fwolff@graniteds.org>
2013-10-09 11:00:19 +0200

    Fix header sections.

--- granite-server-core/src/test/java/org/granite/test/tide/data/Person4.java
+++ granite-server-core/src/test/java/org/granite/test/tide/data/Person4.java
@@ -19,26 +19,6 @@
  *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
  *   USA, or see <http://www.gnu.org/licenses/>.
  */
-/*
-  GRANITE DATA SERVICES
-  Copyright (C) 2011 GRANITE DATA SERVICES S.A.S.
-
-  This file is part of Granite Data Services.
-
-  Granite Data Services is free software; you can redistribute it and/or modify
-  it under the terms of the GNU Library General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or (at your
-  option) any later version.
-
-  Granite Data Services is distributed in the hope that it will be useful, but
-  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Library General Public License
-  for more details.
-
-  You should have received a copy of the GNU Library General Public License
-  along with this library; if not, see <http://www.gnu.org/licenses/>.
-*/
-
 package org.granite.test.tide.data;
 
 import java.util.HashSet;
--- granite-server-hibernate/src/main/java/org/granite/tide/hibernate/Hibernate35DataChangePublishListener.java
+++ granite-server-hibernate/src/main/java/org/granite/tide/hibernate/Hibernate35DataChangePublishListener.java
@@ -19,26 +19,6 @@
  *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
  *   USA, or see <http://www.gnu.org/licenses/>.
  */
-/*
-  GRANITE DATA SERVICES
-  Copyright (C) 2011 GRANITE DATA SERVICES S.A.S.
-
-  This file is part of Granite Data Services.
-
-  Granite Data Services is free software; you can redistribute it and/or modify
-  it under the terms of the GNU Library General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or (at your
-  option) any later version.
-
-  Granite Data Services is distributed in the hope that it will be useful, but
-  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Library General Public License
-  for more details.
-
-  You should have received a copy of the GNU Library General Public License
-  along with this library; if not, see <http://www.gnu.org/licenses/>.
-*/
-
 package org.granite.tide.hibernate;
 
 import static org.granite.util.Reflections.get;
@@ -80,7 +60,7 @@ import org.hibernate.event.def.DefaultFlushEntityEventListener;
 import org.hibernate.persister.entity.EntityPersister;
 
 /**
- * @author William Dra�
+ * @author William DRAI
  */
 public class Hibernate35DataChangePublishListener implements PostInsertEventListener, PostUpdateEventListener,
         PostDeleteEventListener, PreCollectionUpdateEventListener, FlushEntityEventListener {
