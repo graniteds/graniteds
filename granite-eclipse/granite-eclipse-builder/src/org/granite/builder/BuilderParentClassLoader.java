11cb38fac824f1d31b05b7f237ed328979ff9038
William Drai <william.drai@graniteds.org>
2014-01-02 16:14:49 +0100

    GDS-1227

---
+++ granite-eclipse/granite-eclipse-builder/src/org/granite/builder/BuilderParentClassLoader.java
@@ -0,0 +1,40 @@
+/**
+ *   GRANITE DATA SERVICES
+ *   Copyright (C) 2006-2013 GRANITE DATA SERVICES S.A.S.
+ *
+ *   This file is part of the Granite Data Services Platform.
+ *
+ *   Granite Data Services is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2.1 of the License, or (at your option) any later version.
+ *
+ *   Granite Data Services is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+ *   General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the Free Software
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+ *   USA, or see <http://www.gnu.org/licenses/>.
+ */
+
+package org.granite.builder;
+
+/**
+ * @author Franck WOLFF
+ */
+public class BuilderParentClassLoader extends ClassLoader {
+
+	public BuilderParentClassLoader() {
+		super();
+	}
+
+	@Override
+	protected Class<?> findClass(String name) throws ClassNotFoundException {
+		if (name.startsWith("javax.persistence.") || name.startsWith("javax.jdo.") || name.startsWith("org.granite."))
+			return getClass().getClassLoader().loadClass(name);
+		throw new ClassNotFoundException(name);
+	}
+}
