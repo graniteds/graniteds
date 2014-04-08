56190e167e3ca6c11f90b73cbe89c44f7985d97f
William Drai <william.drai@graniteds.org>
2014-04-08 15:08:47 +0200

    GDS-1274

---
+++ granite-server-spring/src/test/java/org/granite/test/tide/spring/service/Hello3Controller.java
@@ -0,0 +1,45 @@
+/**
+ *   GRANITE DATA SERVICES
+ *   Copyright (C) 2006-2014 GRANITE DATA SERVICES S.A.S.
+ *
+ *   This file is part of the Granite Data Services Platform.
+ *
+ *   Granite Data Services is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2.1 of the License, or (at your option) any later version.
+ *
+ *   Granite Data Services is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+ *   General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the Free Software
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+ *   USA, or see <http://www.gnu.org/licenses/>.
+ */
+package org.granite.test.tide.spring.service;
+
+import org.granite.messaging.service.annotations.RemoteDestination;
+import org.granite.test.tide.data.Contact;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+@RemoteDestination
+public class Hello3Controller {
+    
+	@RequestMapping(value="/hello3/hello")
+    public String hello(@RequestBody Contact contact) {
+        return "Hello " + contact.getEmail();
+    }
+    
+	@RequestMapping(value="/hello3/hello2")
+    public String hello2(@RequestBody Contact contact, @RequestParam("greet") String greet) {
+        return greet + " " + contact.getEmail();
+    }
+}
