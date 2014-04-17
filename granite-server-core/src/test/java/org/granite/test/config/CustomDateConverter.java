f2f5dcb51b1bb69230b5287576d9d033d3094118
fwolff <fwolff@graniteds.org>
2014-04-17 17:13:10 +0200

    Fix custom converter test.

--- granite-server-core/src/test/java/org/granite/test/config/CustomDateConverter.java
+++ granite-server-core/src/test/java/org/granite/test/config/CustomDateConverter.java
@@ -22,7 +22,6 @@
 package org.granite.test.config;
 
 import java.lang.reflect.Type;
-import java.util.Calendar;
 import java.util.Date;
 
 import org.granite.messaging.amf.io.convert.Converter;
@@ -38,16 +37,11 @@ public class CustomDateConverter extends Converter implements Reverter {
 	
 	@Override
 	protected boolean internalCanConvert(Object value, Type targetType) {
-        Class<?> targetClass = TypeUtil.classOfType(targetType);
-        return (
-            targetClass.isAssignableFrom(Date.class) ||
-            targetClass.isAssignableFrom(Calendar.class) ||
-            targetClass.isAssignableFrom(java.sql.Timestamp.class) ||
-            targetClass.isAssignableFrom(java.sql.Time.class) ||
-            targetClass.isAssignableFrom(java.sql.Date.class)
-        ) && (
-            value == null || value instanceof Date
-        );
+		if (value == null)
+			return true;
+		if (targetType != Long.class)
+			return false;
+		return Date.class.isAssignableFrom(TypeUtil.classOfType(value.getClass()));
 	}
 
 	@Override
